ec2c596619ba008d811d23f184c9e0af0424b6eb
[Emils Glotovs] <[emilsglotovs@sk.lvt.lvgit config user.emailgit config --global user.email [emilsgltoovs@sk.lvt.lv]git config user.emailgit config --global use.email[emils.glotovs@sk.lvt.lv>
2026-01-14 09:50:10 +0200

    Daļēji pabeidzu Pasūtījuma klasi

--- src/lvt_pd/Pasutijums.java
+++ src/lvt_pd/Pasutijums.java
@@ -1,6 +1,8 @@
 package lvt_pd;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Pasutijums {
 
@@ -12,44 +14,63 @@ public class Pasutijums {
     private LocalDateTime pasutijumaLaiks;
     private Klients klients;
     private PasutijumaStatus statuss;
-    private String piegadesVeids; // "UZ_VIETAS" vai "PIEGADE"
+    private double kopsumma; 
+    private String piegadesVeids;
+    private List<Pica> picas = new ArrayList<>();
 
     public Pasutijums(int pasutijumaID, Klients klients, String piegadesVeids) {
-
         this.pasutijumaID = pasutijumaID;
         this.klients = klients;
         this.piegadesVeids = piegadesVeids;
         this.pasutijumaLaiks = LocalDateTime.now();
         this.statuss = PasutijumaStatus.PIEŅEMTS;
+        this.kopsumma = 0;
     }
 
-    public int getPasutijumaID() {
-return pasutijumaID;
-}
+    public int getPasutijumaID() { 
+    	return pasutijumaID; 
+    	}
     public LocalDateTime getPasutijumaLaiks() {
- return pasutijumaLaiks;
- }
-    public Klients getKlients() { 
-return klients; 
-}
+    	return pasutijumaLaiks;
+    	}
+    public Klients getKlients() {
+    	return klients;
+    	}
 
     public PasutijumaStatus getStatuss() {
- return statuss;
- }
+    	return statuss; 
+    	}
     public void setStatuss(PasutijumaStatus statuss) { 
-this.statuss = statuss; 
-}
+    	this.statuss = statuss;
+    	}
 
-    public String getPiegadesVeids() { 
-return piegadesVeids;
- }
-    public void setPiegadesVeids(String piegadesVeids) { 
-this.piegadesVeids = piegadesVeids;
- }
+    public double getKopsumma() {
+    	return kopsumma; 
+    	} // pagaidām vienmēr 0
+
+    public String getPiegadesVeids() { return piegadesVeids; }
+    public void setPiegadesVeids(String piegadesVeids) { this.piegadesVeids = piegadesVeids; }
+
+    public List<Pica> getPicas() { return picas; }
+
+    public void pievienotPreci(Pica pica) {
+        if (pica != null) {
+            picas.add(pica);
+            
+        }
+    }
+
+    public void nonemtPreci(int index) {
+        if (index >= 0 && index < picas.size()) {
+            picas.remove(index);
+            
+        }
+    }
 
     public String IsaInfo() {
         return "ID: " + pasutijumaID
                 + " | " + statuss
+                + " | Picas: " + picas.size()
                 + " | Klients: " + klients.getVards();
     }
 }
